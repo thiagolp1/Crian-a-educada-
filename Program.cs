a9d817a2eb8670e3f20b2973f1e935abea1e155f
thiago <e107ti@cps.sp.gov.br>
2022-03-18 21:24:24 -0300

    terminei a licao

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿String Pcensurada = "#@$%*!&";
+String FDigitada, Fcensurada;
+
+Console.Write("Digite uma frase: ");
+FDigitada = Console.ReadLine()!;
+Fcensurada = FDigitada
+
+.Replace("chato", Pcensurada)
+.Replace("chata", Pcensurada)
+.Replace("bobo", Pcensurada)
+.Replace("boba", Pcensurada)
+.Replace("feio", Pcensurada)
+.Replace("feia", Pcensurada)
+.Replace("boboca", Pcensurada)
+.Replace("bocó", Pcensurada)
+.Replace("tonto", Pcensurada)
+.Replace("tonta", Pcensurada)
+.Replace("palerma", Pcensurada)
+.Replace("paspalho", Pcensurada)
+.Replace("paspalha", Pcensurada)
+.Replace("tantã", Pcensurada)
+.Replace("panaca", Pcensurada)
+.Replace("pentelho", Pcensurada)
+.Replace("pentelha", Pcensurada)
+.Replace("burro", Pcensurada)
+.Replace("burra", Pcensurada)
+.Replace("besta", Pcensurada)
+.Replace("CHATO", Pcensurada)
+.Replace("CHATA", Pcensurada)
+.Replace("BOBO", Pcensurada)
+.Replace("BOBA", Pcensurada)
+.Replace("FEIO", Pcensurada)
+.Replace("FEIA", Pcensurada)
+.Replace("BOBOCA",   Pcensurada)
+.Replace("BOCÓ", Pcensurada)
+.Replace("TONTO", Pcensurada)
+.Replace("TONTA", Pcensurada)
+.Replace("PALERMA", Pcensurada)
+.Replace("PASPALHO",Pcensurada)
+.Replace("PASPALHA",Pcensurada)
+.Replace("TANTÃ", Pcensurada)
+.Replace("PANACA", Pcensurada)
+.Replace("PENTELHO",Pcensurada)
+.Replace("PENTELHA",Pcensurada)
+.Replace("BURRO", Pcensurada)
+.Replace("BURRA", Pcensurada)
+.Replace("BESTA", Pcensurada);
+
+Console.WriteLine(Fcensurada);
